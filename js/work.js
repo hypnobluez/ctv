c94792245ffecd7906c795c3b31dd0d40e689a4d
hypnobluez <hypnobluez@gmail.com>
2019-01-12 14:16:36 +1100

    included grid

---
+++ js/work.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $(".collapse").on("shown.bs.collapse", function() {
+    $(this)
+      .parent()
+      .find(".glyphicon-plus")
+      .removeClass("glyphicon-plus")
+      .addClass("glyphicon-minus");
+  });
+});
+$(document).ready(function() {
+  $(".collapse").on("hidden.bs.collapse", function() {
+    $(this)
+      .parent()
+      .find(".glyphicon-minus")
+      .removeClass("glyphicon-minus")
+      .addClass("glyphicon-plus");
+  });
+});
