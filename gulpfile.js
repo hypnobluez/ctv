733f6886e9f2fb12a4508dcc9d42f49f678aa1d3
hypnobluez <hypnobluez@gmail.com>
2019-01-27 12:53:24 +1100

    cleaned up gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,16 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
+var del = require('del');
+var cleanCSS = require('gulp-clean-css');
+var rename = require("gulp-rename");
 
-// Compile sass into CSS & auto-inject into browsers
+//CLEAN CSS FOLDER
+gulp.task('clean', function () {
+    return del(['src/css/**/*.css']);
+});
+
+// COMPILE SASS INTO CSS
 gulp.task('sass', function () {
     return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
         .pipe(sass())
@@ -10,15 +18,39 @@ gulp.task('sass', function () {
         .pipe(browserSync.stream());
 });
 
-// Move the javascript files into our /src/js folder
+// COPY OVER BOOTSTRAP JS FILES INTO SRC/JS
 gulp.task('js', function () {
     return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
         .pipe(gulp.dest("src/js"))
         .pipe(browserSync.stream());
 });
 
-// Static Server + watching scss/html files
-gulp.task('serve', gulp.series('sass', function () {
+//COPY OVER FONT AWESOME FILES NITO FONTS/
+gulp.task('fonts', function () {
+    return gulp.src([
+        './node_modules/font-awesome/**/*',
+        '!./node_modules/font-awesome/{less,less/*}',
+        '!./node_modules/font-awesome/{scss,scss/*}',
+        '!./node_modules/font-awesome/.*',
+        '!./node_modules/font-awesome/*.{txt,json,md}'
+    ])
+        .pipe(gulp.dest('./src/fonts/font-awesome'))
+});
+
+// MINIFY CSS
+gulp.task('css:minify', function cssMinify() {
+    return gulp.src('./src/css/**/*.css')
+        .pipe(cleanCSS())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('./src/css'))
+        .pipe(browserSync.stream());
+});
+
+
+// GULP SERVE FOR DEV PURPOSES
+gulp.task('serve', gulp.series('clean', 'sass', 'css:minify', function () {
 
     browserSync.init({
         server: "./src"
@@ -28,4 +60,5 @@ gulp.task('serve', gulp.series('sass', function () {
     gulp.watch("src/**/*.html").on('change', browserSync.reload);
 }));
 
-gulp.task('default', gulp.parallel('js', 'serve'));
+//DEFAULT GULP TASK
+gulp.task('default', gulp.parallel('js', 'fonts', 'serve'));
